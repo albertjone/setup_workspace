a2181d88eda8b0d6a9edceeb265c79ec768ea9f1
xiaojueguan <xiaojueguan@gmail.com>
2020-01-09 16:19:15 +0800

    Thu Jan  9 16:19:15 CST 2020

--- main.sh
+++ main.sh
@@ -140,6 +140,22 @@ function install_brew_softwares() {
     software_is_installed telnet
   fi
 
+  is_brew_software_installed pkg-config
+  installed=$?
+  if [ $installed == 0 ]; then
+    brew install pkg-config
+  else
+    software_is_installed pkg-config
+  fi
+
+  is_brew_software_installed libvirt
+  install=$?
+  if [ $installed == 0 ]; then
+    brew install libvirt
+  else
+    software_is_installed libvirt
+  fi
+
   unset_http_https_proxy
 }
 
@@ -183,91 +199,6 @@ function git_clone_company_repo_to_work() {
   git clone git@gitlab.sh.99cloud.net:openstack/neutron.git ~/code/Work/neutron
   git clone git@gitlab.sh.99cloud.net:openstack/kolla-ansible.git ~/code/Work/kolla-ansible
 
-}
-
-function setup_zshrc() {
-  # bakup your zsh configuration
-  if [ -f ~/.zshrc ]
-    then
-      mv ~/.zshrc ~/.zshrc.bak$(date "+%Y-%m-%d%H:%M:%S")
-  fi
-
-  cat << EOF > ~/.zshrc
-source /usr/local/opt/powerlevel9k/powerlevel9k.zsh-theme
-
-POWERLEVEL9K_CUSTOM_WIFI_SIGNAL="zsh_wifi_signal"
-POWERLEVEL9K_CUSTOM_WIFI_SIGNAL_BACKGROUND="white"
-POWERLEVEL9K_CUSTOM_WIFI_SIGNAL_FOREGROUND="black"
-
-zsh_wifi_signal(){
-        local output=\$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I)
-        local airport=\$(echo \$output | grep 'AirPort' | awk -F': ' '{print \$2}')
-
-        if [ "\$airport" = "Off" ]; then
-                local color='%F{black}'
-                echo -n "%{\$color%}Wifi Off"
-        else
-                local ssid=\$(echo \$output | grep ' SSID' | awk -F': ' '{print \$2}')
-                local speed=\$(echo \$output | grep 'lastTxRate' | awk -F': ' '{print \$2}')
-                local color='%F{black}'
-
-                [[ \$speed -gt 100 ]] && color='%F{black}'
-                [[ \$speed -lt 50 ]] && color='%F{red}'
-
-                echo -n "%{\$color%}\$speed Mbps \uf1eb%{%f%}" # removed char not in my PowerLine font
-        fi
-}
-
-POWERLEVEL9K_CONTEXT_TEMPLATE='%n'
-POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND='white'
-POWERLEVEL9K_BATTERY_CHARGING='yellow'
-POWERLEVEL9K_BATTERY_CHARGED='green'
-POWERLEVEL9K_BATTERY_DISCONNECTED='$DEFAULT_COLOR'
-POWERLEVEL9K_BATTERY_LOW_THRESHOLD='10'
-POWERLEVEL9K_BATTERY_LOW_COLOR='red'
-POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
-POWERLEVEL9K_BATTERY_ICON='\uf1e6 '
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{014}\u2570%F{cyan}\uF460%F{073}\uF460%F{109}\uF460%f "
-POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='yellow'
-POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND='yellow'
-POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
-
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context battery dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time dir_writable ip custom_wifi_signal ram load background_jobs)
-
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-
-POWERLEVEL9K_TIME_FORMAT="%D{\uf017 %H:%M \uf073 %d/%m/%y}"
-POWERLEVEL9K_TIME_BACKGROUND='white'
-POWERLEVEL9K_RAM_BACKGROUND='yellow'
-POWERLEVEL9K_LOAD_CRITICAL_BACKGROUND="white"
-POWERLEVEL9K_LOAD_WARNING_BACKGROUND="white"
-POWERLEVEL9K_LOAD_NORMAL_BACKGROUND="white"
-POWERLEVEL9K_LOAD_CRITICAL_FOREGROUND="red"
-POWERLEVEL9K_LOAD_WARNING_FOREGROUND="yellow"
-POWERLEVEL9K_LOAD_NORMAL_FOREGROUND="black"
-POWERLEVEL9K_LOAD_CRITICAL_VISUAL_IDENTIFIER_COLOR="red"
-POWERLEVEL9K_LOAD_WARNING_VISUAL_IDENTIFIER_COLOR="yellow"
-POWERLEVEL9K_LOAD_NORMAL_VISUAL_IDENTIFIER_COLOR="green"
-POWERLEVEL9K_HOME_ICON=''
-POWERLEVEL9K_HOME_SUB_ICON=''
-POWERLEVEL9K_FOLDER_ICON=''
-POWERLEVEL9K_STATUS_VERBOSE=true
-POWERLEVEL9K_STATUS_CROSS=true
-
-# cause the python installed by homebrew will be placed to /usr/local/bin
-# Todo(xiaojueguan) this line will fail in deploy
-export PATH="/usr/local/bin:/usr/local/sbin:${PATH}"
-
-alias swork="cd ~/code/Work"
-alias sint="cd ~/code/Interest"
-alias srep="cd ~/code/Openstack"
-alias sproxy="export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;"
-alias dproxy="unset http_proxy;unset https_proxy"
-alias stack="ssh root@192.168.199.126"
-
-EOF
 }
 
 function git_clone_my_repos_to_interets() {
@@ -325,18 +256,16 @@ EOF
 }
 
 function main() {
-  install_brew_softwares
-  setup_zshrc
+  # install_brew_softwares
+  # setup_zshrc
   setup_pip_rep
-  setup_code_directory
-  git_clone_company_repo_to_work
-  git_clone_my_repos_to_interets
-  setup_virtualenvwrappers
-  create_virtualenvwrappers
-  config_git
-  setup_config_for_vscode
+  # setup_code_directory
+  # git_clone_company_repo_to_work
+  # git_clone_my_repos_to_interets
+  # setup_virtualenvwrappers
+  # create_virtualenvwrappers
+  # config_git
+  # setup_config_for_vscode
 }
 
 main
-
-
